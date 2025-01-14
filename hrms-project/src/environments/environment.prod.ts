003f7f2e40932e646436270914009860e67fe431
zeo <you@example.com>
2025-01-14 11:07:58 +0530

    ipchnage

--- hrms-project/src/environments/environment.prod.ts
+++ hrms-project/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://80.65.208.178:8000', // Live API URL use
+    apiBaseUrl: 'http://80.65.208.178', // Live API URL use
 };
